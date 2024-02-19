eace5a113eae4f5d7735b85a417e0371d3a288d6
Bram <bram.robyn@homail.com>
2024-02-19 11:48:33 +0100

    first commit

---
+++ Minigin/GameObject.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <memory>
+#include "Transform.h"
+
+namespace dae
+{
+	class Texture2D;
+
+	// todo: this should become final.
+	class GameObject 
+	{
+	public:
+		virtual void Init();
+		virtual void Update();
+		virtual void LateUpdate();
+		virtual void Render() const;
+
+		void SetTexture(const std::string& filename);
+		void SetPosition(float x, float y);
+
+		GameObject() = default;
+		virtual ~GameObject();
+		GameObject(const GameObject& other) = delete;
+		GameObject(GameObject&& other) = delete;
+		GameObject& operator=(const GameObject& other) = delete;
+		GameObject& operator=(GameObject&& other) = delete;
+
+	private:
+		Transform m_transform{};
+		// todo: mmm, every gameobject has a texture? Is that correct?
+		std::shared_ptr<Texture2D> m_texture{};
+	};
+}
